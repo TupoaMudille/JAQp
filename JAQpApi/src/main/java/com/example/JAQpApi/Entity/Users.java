7eef87e16925d1817a654549e992e5c434389ecb
SigachevAV <ton666013@gmail.com>
2023-12-23 21:48:56 +0300

    update user settings controller
     * add get endpoint to get all info about user exept password
     * add setings set methods for first name, last nabe, second name and birth date

--- JAQpApi/src/main/java/com/example/JAQpApi/Entity/Users.java
+++ JAQpApi/src/main/java/com/example/JAQpApi/Entity/Users.java
@@ -17,7 +17,7 @@ public class Users
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String username;
 
     @Column(nullable = false)
@@ -39,7 +39,7 @@ public class Users
     private String lastName;
 
     @Column
-    private LocalDate burthDate;
+    private OffsetDateTime burthDate;
 
 //    @OneToMany(mappedBy = "owner")
 //    private Set<Quez> ownerQuezes;
