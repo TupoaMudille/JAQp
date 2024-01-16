1836cccb8c1134e55e499c5cdeb2a612c2a10ebd
TupoaMudille <jeffersonia772@gmail.com>
2024-01-16 23:25:13 +0300

    fix: reg action+validation

--- JAQpFront/src/http/userApi.js
+++ JAQpFront/src/http/userApi.js
@@ -2,11 +2,20 @@ import { apiHost } from "./apiIndex";
 const userBase = "users/"
 export const LoginUser = async (_username, _password) =>
 {
+    try{
     const {data} = await apiHost.get(userBase+"login?password="+_password+"&username="+_username);
     return data;
+    }
+    catch(e)
+    {
+        console.log(e);
+    }
 }
 export const RegisterUser = async (_username, _password) =>
 {
-    const {data} = await apiHost.post(userBase+"register?password="+_password+"&username="+_username);
-    return data;
+    let result = true;
+    await apiHost.post(userBase+"register?password="+_password+"&username="+_username).catch(function(error) {console.log(error); result = false});
+    return result;
 }
+
+export default RegisterUser;
